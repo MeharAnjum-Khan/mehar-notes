4b37d8326d02fc5cf5b4ea14697fdf4c5d972b49
MeherAnjum-Khan <Khanmehar5555@gmail.com>
2025-02-28 09:30:13 +0530

    my notes updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
-import './App.css';
+import { useEffect, useState } from 'react';
+import Navbar from './Components/Navbar';
 import Notes from './Components/Notes';
 
+
 function App() {
- 
+  const [dark, setDark] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
+
+
+  // dark mode function
+  const toggleDarkMode = () => {
+    setDark(prevDark => !prevDark);
+    document.body.style.backgroundColor = dark ? "white" : "black";
+    document.body.style.color = dark ? "black" : "green";
+  };
 
   return (
-    <div>
-
+    <div className={dark ? "dark-mode" : "light-mode"}>
+      <Navbar dark={dark} toggleDarkMode={toggleDarkMode} searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
       
-      <Notes/>
+      
+      <Notes searchTerm={searchTerm}/>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
